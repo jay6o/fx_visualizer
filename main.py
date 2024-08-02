51880857048fc7706bd2da2d561ebe1bafed489c
Jonathan Urias <ih8merta@gmail.com>
2024-08-02 17:26:10 -0400

    made sequential function calls

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-#! /Library/Frameworks/Python.framework/Versions/3.12/bin/python3
 #%%
-import requests, json
+import requests, json, pandas, mplfinance
 from modules.get_data import get_data
+from modules.preprocess import preprocess
+from modules.visualize import visualize
 
 if __name__ == "__main__":
     get_data(requests, json)
-
+    df = preprocess(json, pandas, 'gj_2010_2024_M.json')
+    visualize(df, mplfinance)
 # %%
