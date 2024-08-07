92408f6c37257da8ec6453f822ffc03fd14a015b
Jonathan Urias <ih8merta@gmail.com>
2024-08-07 19:52:09 -0400

    made menu output shorter by only using major pairs

--- modules/menu/pair_select.py
+++ modules/menu/pair_select.py
@@ -4,7 +4,7 @@ from ..helpers.clear_out import clear_out
 def pair_select(name, system):
     try:
         clear_out(name, system)
-        pair = int(input("--Select a pair--\n==MAJOR PAIRS==\n[1] AUD/CAD\n[2] AUD/JPY\n[3] AUD/USD\n[4] AUD/CHF\n[5] CAD/JPY\n[6] CHF/JPY\n[7] EUR/AUD\n[8] EUR/CAD\n[9] EUR/CHF\n[10] EUR/GBP\n[11] EUR/JPY\n[12] EUR/USD\n[13] GBP/AUD\n[14] GBP/CAD\n[15] GBP/CHF\n[16] GBP/JPY\n[17] GBP/USD\n[18] USD/CAD\n[19] USD/CHF\n[20] USD/CNH\n[21] USD/JPY\n==OTHER==\n[22] USD/CZK (Czech Krone)\n[23] USD/DKK (Danish Krone)\n[24] USD/HKD (Hong Kong Dollar)\n[25] USD/HUF (Hungarian Forint)\n[26] USD/MXN (Mexican Peso)\n[27] USD/NOK (Norwegian Krone)\n[28] USD/PLN (Polish Zloty)\n[29] USD/SEK (Swedish Krona)\n[30] USD/SGD (Singapore Dollar)\n[31] USD/THB (Thai Baht)\n[32] USD/TRY (Turkish Lira)\n[33] USD/ZAR (South African Rand)\n"))
+        pair = int(input("--Select a pair--\n[1] AUD/CAD\n[2] AUD/USD\n[3] EUR/CAD\n[4] EUR/GBP\n[5] EUR/JPY\n[6] EUR/USD\n[7] GBP/CAD\n[8] GBP/JPY\n[9] GBP/USD\n[10] USD/CAD\n[11] USD/CNH\n[12] USD/JPY\n"))
         try:
             pair = pairs[pair]
             return pair
--- modules/menu/pairs.py
+++ modules/menu/pairs.py
@@ -1,34 +1,36 @@
+## To enable any of the pairs, just uncomment and renumber the items. Commenting out less significant pairs was a measure taken to make the menu easier to use, and appeal to the majority.
+
 pairs = {   1: "AUD_CAD",
-            2: "AUD_JPY",
-            3: "AUD_USD",
-            4: "CAD_CHF",
-            5: "CAD_JPY",
-            6: "CHF_JPY",
-            7: "EUR_AUD",
-            8: "EUR_CAD",
-            9: "EUR_CHF",
-            10: "EUR_GBP",
-            11: "EUR_JPY",
-            12: "EUR_USD",
-            13: "GBP_AUD",
-            14: "GBP_CAD",
-            15: "GBP_CHF",
-            16: "GBP_JPY",
-            17: "GBP_USD",
-            18: "USD_CAD",
-            19: "USD_CHF",
-            20: "USD_CNH",
-            21: "USD_JPY",
-            22: "USD_CZK", # Czech Koruna
-            23: "USD_DKK", # Danish Krone
-            24: "USD_HKD", # Hong Kong Dollar
-            25: "USD_HUF", # Hungarian Forint
-            26: "USD_MXN", # Mexican Peso
-            27: "USD_NOK", # Norwegian Krone
-            28: "USD_PLN", # Polish Zloty
-            29: "USD_SEK", # Swedish Krona
-            30: "USD_SGD", # Singapore Dollar
-            31: "USD_THB", # Thai Baht
-            32: "USD_TRY", # Turkish Lira,
-            33: "USD_ZAR", # South African Rand
+          #  2: "AUD_JPY",
+            2: "AUD_USD",
+          #  4: "CAD_CHF",
+          #  5: "CAD_JPY",
+          #  6: "CHF_JPY",
+          #  7: "EUR_AUD",
+            3: "EUR_CAD",
+          #  9: "EUR_CHF",
+            4: "EUR_GBP",
+            5: "EUR_JPY",
+            6: "EUR_USD",
+          #  13: "GBP_AUD",
+            7: "GBP_CAD",
+          #  15: "GBP_CHF",
+            8: "GBP_JPY",
+            9: "GBP_USD",
+            10: "USD_CAD",
+          #  19: "USD_CHF",
+            11: "USD_CNH",
+            12: "USD_JPY",
+          #  22: "USD_CZK", # Czech Koruna
+          #  23: "USD_DKK", # Danish Krone
+          #  24: "USD_HKD", # Hong Kong Dollar
+          #  25: "USD_HUF", # Hungarian Forint
+          #  26: "USD_MXN", # Mexican Peso
+          #  27: "USD_NOK", # Norwegian Krone
+          #  28: "USD_PLN", # Polish Zloty
+          #  29: "USD_SEK", # Swedish Krona
+          #  30: "USD_SGD", # Singapore Dollar
+          #  31: "USD_THB", # Thai Baht
+          #  32: "USD_TRY", # Turkish Lira,
+          #  33: "USD_ZAR", # South African Rand
          }
